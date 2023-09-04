03fa95a7df5f8bbc1b0651aa74a10f15ba4a51b2
SupriyaUppula <128058517+SupriyaUppula@users.noreply.github.com>
2023-09-04 16:28:25 +0530

    Delete photography directory

--- photography/serv1.js
+++
@@ -1,7 +0,0 @@
-const buttons = document.querySelectorAll(".see-more-btn");
-
-buttons.forEach((btn, index) => {
-  btn.addEventListener("click", function() {
-    window.location.href = `another-page-${index + 1}.html`;
-  });
-});
